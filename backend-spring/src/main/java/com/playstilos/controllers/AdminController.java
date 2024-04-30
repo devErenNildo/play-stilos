4eafa4989a64ccecbdc8282e8b649cf4f616161f
DevEren <dev.erenildo@gmail.com>
2024-04-30 15:40:06 -0400

    delete admin

--- backend-spring/src/main/java/com/playstilos/controllers/AdminController.java
+++ backend-spring/src/main/java/com/playstilos/controllers/AdminController.java
@@ -3,7 +3,11 @@ package com.playstilos.controllers;
 import com.playstilos.domain.admin.Admin;
 import com.playstilos.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/admin")
@@ -16,4 +20,16 @@ public class AdminController {
     public Admin creteAdmin(@RequestBody Admin admin){
         return adminService.createAdmin(admin);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAdmin(@PathVariable String id){
+        Optional<Admin> adminOptional = adminService.getById(id);
+        if(adminOptional.isPresent()){
+            adminService.deleteAdmin(id);
+            return ResponseEntity.ok("user successfully deleted");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not found");
+        }
+    }
+
 }
--- backend-spring/src/main/java/com/playstilos/services/AdminService.java
+++ backend-spring/src/main/java/com/playstilos/services/AdminService.java
@@ -5,6 +5,8 @@ import com.playstilos.repositories.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AdminService {
 
@@ -13,5 +15,9 @@ public class AdminService {
 
     public Admin createAdmin(Admin admin){
         return adminRepository.save(admin);
+    }
+
+    public void deleteAdmin(String id){
+        adminRepository.deleteById(id);
     }
 }
